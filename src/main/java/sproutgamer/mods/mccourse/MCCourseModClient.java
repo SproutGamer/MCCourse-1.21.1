57cd8a703cf32448d27cb8def94883e74d6bc194
Benji <mrbenjaminrussell@icloud.com>
2024-08-24 17:27:23 +0100

    Initial commit

---
+++ src/main/java/sproutgamer/mods/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package sproutgamer.mods.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
